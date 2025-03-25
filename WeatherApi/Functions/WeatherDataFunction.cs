fa9dc39e0d17117422be048b925f86315a6b1908
S Salo <hallonapl@outlook.com>
2025-03-25 15:16:48 +0100

    Added functions project

---
+++ WeatherApi/Functions/WeatherDataFunction.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace WeatherApi.Functions
+{
+    public class WeatherDataFunction
+    {
+        private readonly ILogger<IngestionFunction> _logger;
+
+        public WeatherDataFunction(ILogger<IngestionFunction> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("WeatherDataFunction")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
